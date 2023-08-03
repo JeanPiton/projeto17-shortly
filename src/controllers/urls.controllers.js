ba6ff1cb887dec333d09fe7dceb2921f803a3d7b
JeanPiton <jean.orpiton@gmail.com>
2023-08-03 02:15:32 -0300

    Fix: post url/shorten id

--- src/controllers/urls.controllers.js
+++ src/controllers/urls.controllers.js
@@ -7,8 +7,8 @@ export async function urlShorten(req,res){
     const short = nanoid(8)
 
     try {
-        await db.query(`INSERT INTO urls(url,"shortUrl","ownerId") VALUES ($1,$2,$3)`,[url,short,userId])
-        res.status(201).send({id:userId,shortUrl:short})
+        const {rows} = await db.query(`INSERT INTO urls(url,"shortUrl","ownerId") VALUES ($1,$2,$3) RETURNING id`,[url,short,userId])
+        res.status(201).send({id:rows[0].id,shortUrl:short})
     } catch (err) {
         res.status(500).send(err.message)
     }
