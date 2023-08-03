e3b84f8191f78b9ed74c3fed5e3d89fa2ebac165
JeanPiton <jean.orpiton@gmail.com>
2023-08-03 00:50:23 -0300

    Fix: post /signup hash password

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,10 +1,12 @@
 import { db } from "../database/database.connection.js";
+import bcrypt from "bcrypt"
 
 export async function signUp(req,res){
     const {name,email,password} = req.body
+    const hash = bcrypt.hashSync(password,10)
 
     try {
-        await db.query(`INSERT INTO users(name,email,password) VALUES ($1,$2,$3)`,[name,email,password])
+        await db.query(`INSERT INTO users(name,email,password) VALUES ($1,$2,$3)`,[name,email,hash])
         res.sendStatus(201)
     } catch (err) {
         if(err.code == '23505') return res.status(409).send("email alredy registered")
